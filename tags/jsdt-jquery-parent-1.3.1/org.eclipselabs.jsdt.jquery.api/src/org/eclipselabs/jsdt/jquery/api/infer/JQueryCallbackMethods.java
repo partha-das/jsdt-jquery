0a5b27d957a89e1bf535ddfda383132fa133832f
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2011-12-16 10:01:24 +0000

    [maven-release-plugin]  copy for tag jsdt-jquery-parent-1.3.1

---
+++ tags/jsdt-jquery-parent-1.3.1/org.eclipselabs.jsdt.jquery.api/src/org/eclipselabs/jsdt/jquery/api/infer/JQueryCallbackMethods.java
@@ -0,0 +1,77 @@
+package org.eclipselabs.jsdt.jquery.api.infer;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class JQueryCallbackMethods {
+
+  public static final int NO_CALLBACK = -1;
+
+  private final Map<CallbackLocator, Set<Integer>> callbackFunctions;
+  private final Set<String> callbackSelectors;
+
+  public JQueryCallbackMethods() {
+    this.callbackFunctions = new ConcurrentHashMap<CallbackLocator, Set<Integer>>();
+    // REVIEW ConcurrentHashSet?
+    this.callbackSelectors = new HashSet<String>();
+  }
+
+  public void addCallbackMethod(String selector, int argumentAcount, int eventIndex) {
+    this.callbackSelectors.add(selector);
+    CallbackLocator key = new CallbackLocator(selector, argumentAcount);
+    Set<Integer> indices = this.callbackFunctions.get(key);
+    if (indices == null) {
+      indices = new HashSet<Integer>(3);
+      this.callbackFunctions.put(key, indices);
+    }
+    indices.add(eventIndex);
+  }
+
+  public Set<Integer> getCallbackIndices(String selector, int argumentCount) {
+    CallbackLocator key = new CallbackLocator(selector, argumentCount);
+    return this.callbackFunctions.get(key);
+  }
+
+  public boolean isEventSelector(String selector) {
+    return this.callbackSelectors.contains(selector);
+  }
+
+  static final class CallbackLocator {
+
+    final String selector;
+    final int argmentCount;
+
+    CallbackLocator(String selector, int argmentCount) {
+      super();
+      this.selector = selector;
+      this.argmentCount = argmentCount;
+    }
+
+    @Override
+    public String toString() {
+      return this.selector + ":(" + this.argmentCount + ')';
+    }
+
+    @Override
+    public int hashCode() {
+      return this.selector.hashCode() ^ this.argmentCount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (!(obj instanceof CallbackLocator)) {
+        return false;
+      }
+      CallbackLocator other = (CallbackLocator) obj;
+      return this.selector.equals(other.selector)
+          && this.argmentCount == other.argmentCount;
+    }
+
+  }
+
+}
---
+++ tags/jsdt-jquery-parent-1.3.1/org.eclipselabs.jsdt.jquery.api/src/org/eclipselabs/jsdt/jquery/api/infer/JQueryEventInferenceProvider.java
@@ -0,0 +1,60 @@
+package org.eclipselabs.jsdt.jquery.api.infer;
+
+import org.eclipse.wst.jsdt.core.infer.IInferEngine;
+import org.eclipse.wst.jsdt.core.infer.IInferenceFile;
+import org.eclipse.wst.jsdt.core.infer.InferrenceProvider;
+import org.eclipse.wst.jsdt.core.infer.RefactoringSupport;
+import org.eclipse.wst.jsdt.core.infer.ResolutionConfiguration;
+
+public class JQueryEventInferenceProvider implements InferrenceProvider {
+
+  private static final String ID = JQueryEventInferenceProvider.class.getName();
+
+  private final JQueryCallbackMethods callbackMethods;
+
+  public JQueryEventInferenceProvider() {
+    this.callbackMethods = new JQueryCallbackMethodsGenerator().createCallbackMethods();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public IInferEngine getInferEngine() {
+    return new JQueryEventInferEngine(this.callbackMethods);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int applysTo(IInferenceFile scriptFile) {
+    //FIXME
+    return InferrenceProvider.MAYBE_THIS;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getID() {
+    return ID;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResolutionConfiguration getResolutionConfiguration() {
+    return new ResolutionConfiguration();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public RefactoringSupport getRefactoringSupport() {
+    return null;
+  }
+
+}
