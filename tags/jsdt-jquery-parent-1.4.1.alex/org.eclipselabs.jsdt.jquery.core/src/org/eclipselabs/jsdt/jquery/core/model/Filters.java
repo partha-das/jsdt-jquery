beed6f2ba7957634805dccd2d0f0f07b7831da4e
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2012-04-12 05:17:56 +0000

    [maven-release-plugin]  copy for tag jsdt-jquery-parent-1.4.1.alex

---
+++ tags/jsdt-jquery-parent-1.4.1.alex/org.eclipselabs.jsdt.jquery.core/src/org/eclipselabs/jsdt/jquery/core/model/Filters.java
@@ -0,0 +1,117 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2011 Philippe Marschall and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Philippe Marschall
+ * *****************************************************************************
+ */
+package org.eclipselabs.jsdt.jquery.core.model;
+
+import org.eclipselabs.jsdt.jquery.core.api.JQueryMember;
+
+enum Filters implements Predicate {
+
+
+
+  FUNCTION {
+
+    @Override
+    public boolean isTrue(Function function) {
+      return true;
+    }
+
+    @Override
+    public boolean isTrue(Property property) {
+      return false;
+    }
+
+  },
+
+  PROPERTY {
+
+    @Override
+    public boolean isTrue(Function function) {
+      return false;
+    }
+
+    @Override
+    public boolean isTrue(Property property) {
+      return true;
+    }
+
+  },
+
+  CLASS_SIDE {
+
+    @Override
+    public boolean isTrue(Function function) {
+      return isJQueryStatic(function);
+    }
+
+    @Override
+    public boolean isTrue(Property property) {
+      return isJQueryStatic(property);
+    }
+
+  },
+
+  INSTANCE_SIDE {
+
+    @Override
+    public boolean isTrue(Function function) {
+      return isJQueryObject(function);
+    }
+
+    @Override
+    public boolean isTrue(Property property) {
+      return isJQueryObject(property);
+    }
+
+  },
+
+  EVENT {
+
+    @Override
+    public boolean isTrue(Function function) {
+      return isJQueryEvent(function);
+    }
+
+    @Override
+    public boolean isTrue(Property property) {
+      return isJQueryEvent(property);
+    }
+
+  };
+
+
+  static boolean isJQueryObject(DocumentedMember member) {
+    return isJQueryObject(member.getOwner());
+  }
+
+  static boolean isJQueryObject(String owner) {
+    return JQueryMember.JQUERY_OBJECT.equals(owner);
+  }
+
+  static boolean isJQueryStatic(DocumentedMember member) {
+    return isJQueryStatic(member.getOwner());
+  }
+
+  static boolean isJQueryStatic(String owner) {
+    return "jQuery".equals(owner);
+  }
+
+  static boolean isJQueryEvent(DocumentedMember member) {
+    return isJQueryEvent(member.getOwner());
+  }
+
+  static boolean isJQueryEvent(String owner) {
+    return "event".equals(owner);
+  }
+
+
+}
