5e9ea219177f2e2251071cb34cf3f52526aed696
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2014-03-23 10:42:04 +0000

    [maven-release-plugin]  copy for tag jsdt-jquery-parent-1.7.0

---
+++ tags/jsdt-jquery-parent-1.7.0/org.eclipselabs.jsdt.jquery.core/src/org/eclipselabs/jsdt/jquery/core/model/FunctionArgument.java
@@ -0,0 +1,45 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2011 Philippe Marschall and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Philippe Marschall
+ * *****************************************************************************
+ *
+ */
+package org.eclipselabs.jsdt.jquery.core.model;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.eclipselabs.jsdt.jquery.core.api.JQueryArgument;
+
+
+
+class FunctionArgument extends Argument {
+
+  private final boolean optional;
+  private final Collection<Option> options;
+
+  FunctionArgument(String name, Set<String> types, String description, boolean optional, String defaultValue, Collection<Option> options) {
+    super(name, types, description, defaultValue);
+    this.optional = optional;
+    this.options = options;
+  }
+
+  @Override
+  public boolean isOptional() {
+    return this.optional;
+  }
+
+  @Override
+  public Collection<? extends JQueryArgument> getOptions() {
+    // TODO Auto-generated method stub
+    return this.options;
+  }
+
+}
